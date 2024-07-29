7da45e93f3ebe8c2ff1dac995154507c134beef9
ErikaContreras15 <econtrerasz@est.ups.edu.ec>
2024-07-29 11:36:42 -0500

    correoService
    No se conecta el visual con EmailJS

--- integrador/src/app/servicio/correo/correo.service.ts
+++ integrador/src/app/servicio/correo/correo.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import * as emailjs from 'emailjs-com';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CorreoService {
 
+  constructor() {
+    emailjs.init('kJpPLWuM919m4Nk_K');
+  }
+
+  sendEmail(toEmail: string, subject: string, message: string): Promise<emailjs.EmailJSResponseStatus> {
+    const templateParams = {
+      to_email: toEmail,
+      subject: subject,
+      message: message
+    };
+
+    return emailjs.send('service_v7f728j', 'template_s7ilcn2', templateParams);
+  }
 }
